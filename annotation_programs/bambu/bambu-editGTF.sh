03d42bd42e257fb3d8904d0bc6167183ec2e7c14
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-02-27 09:14:10 +0800

    Create bambu-editGTF.sh

---
+++ annotation_programs/bambu/bambu-editGTF.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+awk -F'\t' '{
+    $9 = gensub(/(transcript_id "[^"]+"); (gene_id "[^"]+");/, "\\2; \\1;", 1, $9);
+    print
+}' OFS='\t' RGT_Planet_v2.gtf > RGT_Planet_v2_BAMBU-edit.gtf
