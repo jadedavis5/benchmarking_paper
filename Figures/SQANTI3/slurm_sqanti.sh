db6559e1dee95a3c4dcb6d507287d2406b1b2cbb
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-02-28 14:01:25 +0800

    Create slurm_sqanti.sh

---
+++ Figures/SQANTI3/slurm_sqanti.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=run-sqanti3
+#SBATCH --account=fl3
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=64
+#SBATCH --time=10:00:00
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --export=ALL
+#SBATCH --mem=40GB
+
+module load singularity/4.1.0-slurm
+
+
+#singularity pull docker://anaconesalab/sqanti3
+reference=/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/RGT_Planet_v2.gtf
+genome=/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/220816_RGT_Planet_pseudomolecules_and_unplaced_contigs_CPclean.fasta
+
+#Convert GFF3 files output from pipeline to GTF
+#for run in STref STnoref BAMBUref BAMBUnoref IQref IQnoref FLAIRref
+#do
+#singularity run /software/projects/fl3/jdavis/setonix/containers/gffread_0.11.7.sif gffread outputAnnotation_${run}.gff3 -T -o GTF/outputAnnotation_${run}.gtf
+#done
+
+for run in STref STnoref BAMBUref BAMBUnoref IQref IQnoref FLAIRref
+do
+singularity run sqanti3_latest.sif sqanti3_qc.py /scratch/fl3/jdavis/final_annotations/final_results/annotations/GTF/outputAnnotation_${run}.gtf $reference $genome -o $run --short_reads shortRead.fofn
+done
