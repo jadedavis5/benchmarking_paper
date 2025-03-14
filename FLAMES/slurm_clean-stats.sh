85496e7a0c735f907c6c006145c73bd4e5090e80
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-03-14 15:53:48 +0800

    Create slurm_clean-stats.sh

---
+++ FLAMES/slurm_clean-stats.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=runpipeline
+#SBATCH --account=fl3
+#SBATCH --partition=work
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00:00
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.err
+#SBATCH --export=ALL
+#SBATCH --mem=10GB
+
+module load nextflow/23.10.0
+
+module load singularity/4.1.0-nompi
+
+nextflow run main.nf -profile pawsey_setonix,singularity --mode 'clean_stats' --gtf_input '../isoform_annotated.gff3' \
+--genome '/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/220816_RGT_Planet_pseudomolecules_and_unplaced_contigs_CPclean.fasta' --ref_annotation '/scratch/fl3/jdavis/REFERENCES/RGT_2_2024/RGT_Planet_v2.gtf'
