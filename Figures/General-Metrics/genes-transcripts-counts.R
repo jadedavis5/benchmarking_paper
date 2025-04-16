c8bdbf66372b19420e47fd88d63d97e8ed02938f
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-04-16 10:41:52 +0800

    Update genes-transcripts-counts.R

--- Figures/General-Metrics/genes-transcripts-counts.R
+++ Figures/General-Metrics/genes-transcripts-counts.R
@@ -1,3 +1,5 @@
+#Genes and transcripts plot
+
 library(ggplot2)
 library(reshape2)
 
@@ -12,28 +14,29 @@ colnames(data_long) <- c("Method", "Category", "Value")
 data_long$Method <- factor(data_long$Method, levels = desired_order)
 
 
- plot <- ggplot(data_long, aes(x = Method, y = Value, fill = Category)) +
-   geom_bar(stat = "identity", position = "dodge") +
-   labs(y = "Count") +
-   theme_minimal() +
-   theme(axis.text.x = element_text(angle = 45, hjust = 1, size=13), axis.text.y = element_text(size = 10),
-         axis.title.y = element_text(face = "bold",size = 15),
-         legend.text = element_text(size = 15),
-         axis.title.x=element_blank(),
-         legend.title = element_text(size = 18)) +
-   
-   
-   geom_segment(aes(x = 1.5, xend = 6.5, y = -2000, yend = -2000), color = "black") +  
-   geom_segment(aes(x = 6.5, xend = 9.5, y = -2000, yend = -2000), color = "black") +  
-   
-   
-   geom_segment(aes(x = 1.5, xend = 1.5, y = -5000, yend = 0), color = "black", size = 0.8) +  #short line for reference guided
-   geom_segment(aes(x = 6.5, xend = 6.5, y = -5000, yend = 0), color = "black", size = 0.8) +  #short line for de novo
-   geom_segment(aes(x = 9.5, xend = 9.5, y = -5000, yend = 0), color = "black", size = 0.8) +
-   
-   annotate("text", x = 4, y = -4000, label = "Reference guided", vjust = 0.5, size = 6) +
-   annotate("text", x = 8, y = -4000, label = "De novo", vjust = 0.5, size = 6, fontface = 'italic') +
-   ylim(-5000, max(data_long$Value))
-
- ggsave(filename = "genes-transcripts-counts.tiff", plot = plot, device = 'tiff', width= 7.66, height= 7.22, dpi = 500)
-
+plot <- ggplot(data_long, aes(x = Method, y = Value, fill = Category)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_x_discrete(labels = gsub("\\.(ref|refFree)", "", desired_order)) +
+  labs(y = "Count", ,
+       x = "Method") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size=13), axis.text.y = element_text(size = 10),
+        axis.title.y = element_text(face = "bold",size=15),
+        axis.title.x = element_text(face = "bold",size=15),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 18)) +
+  
+  
+  geom_segment(aes(x = 1.5, xend = 6.5, y = -2000, yend = -2000), color = "black") +  
+  geom_segment(aes(x = 6.5, xend = 9.5, y = -2000, yend = -2000), color = "black") +  
+  
+  
+  geom_segment(aes(x = 1.5, xend = 1.5, y = -5000, yend = 0), color = "black", size = 0.8) +  #short line for reference guided
+  geom_segment(aes(x = 6.5, xend = 6.5, y = -5000, yend = 0), color = "black", size = 0.8) +  #short line for de novo
+  geom_segment(aes(x = 9.5, xend = 9.5, y = -5000, yend = 0), color = "black", size = 0.8) +
+  
+  annotate("text", x = 4, y = -4000, label = "Reference guided", vjust = 0.5, size = 6) +
+  annotate("text", x = 8, y = -4000, label = "De novo", vjust = 0.5, size = 6, fontface = 'italic') +
+  ylim(-5000, max(data_long$Value))
+plot
+ggsave(filename = "genes-transcripts-counts.tiff", plot = plot, device = 'tiff', width= 7.66, height= 7.22, dpi = 500)
