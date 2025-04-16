de3d0e39be7508903263206706b841f1a12c0632
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-04-16 10:45:20 +0800

    Update genes-transcripts-counts.R

--- Figures/General-Metrics/genes-transcripts-counts.R
+++ Figures/General-Metrics/genes-transcripts-counts.R
@@ -20,7 +20,7 @@ plot <- ggplot(data_long, aes(x = Method, y = Value, fill = Category)) +
   labs(y = "Count", ,
        x = "Method") +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, size=13), axis.text.y = element_text(size = 10),
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size=10), axis.text.y = element_text(size = 10),
         axis.title.y = element_text(face = "bold",size=15),
         axis.title.x = element_text(face = "bold",size=15),
         legend.text = element_text(size = 15),
@@ -35,8 +35,9 @@ plot <- ggplot(data_long, aes(x = Method, y = Value, fill = Category)) +
   geom_segment(aes(x = 6.5, xend = 6.5, y = -5000, yend = 0), color = "black", size = 0.8) +  #short line for de novo
   geom_segment(aes(x = 9.5, xend = 9.5, y = -5000, yend = 0), color = "black", size = 0.8) +
   
-  annotate("text", x = 4, y = -4000, label = "Reference guided", vjust = 0.5, size = 6) +
-  annotate("text", x = 8, y = -4000, label = "De novo", vjust = 0.5, size = 6, fontface = 'italic') +
+  annotate("text", x = 4, y = -4000, label = "Reference guided", vjust = 0.5, size = 5) +
+  annotate("text", x = 8, y = -4000, label = "De novo", vjust = 0.5, size = 5, fontface = 'italic') +
   ylim(-5000, max(data_long$Value))
 plot
 ggsave(filename = "genes-transcripts-counts.tiff", plot = plot, device = 'tiff', width= 7.66, height= 7.22, dpi = 500)
+
