ffd07e44acd7fbc94eefc2b01f0e69428b22be00
jadedavis5 <111946376+jadedavis5@users.noreply.github.com>
2025-04-16 10:48:06 +0800

    Update splicing_profiles.R

--- Figures/General-Metrics/splicing_profiles.R
+++ Figures/General-Metrics/splicing_profiles.R
@@ -1,3 +1,5 @@
+#Transcript splicing
+
 library(ggplot2)
 library(reshape2)
 library(RColorBrewer)
@@ -22,13 +24,14 @@ palette <- brewer.pal(n = 3, name = "YlOrRd")
 plot <- ggplot(data_long, aes(x = Method, y = Percentage, fill = Category)) +
   geom_bar(stat = "identity") +
   labs( 
-       x = "Annotation method", 
-       y = "Percentage of total transcripts") +
+    x = "Method", 
+    y = "Percentage of total transcripts") +
   scale_fill_manual(values = palette) +  
+  scale_x_discrete(labels = gsub("\\.(ref|refFree)", "", desired_order)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle=45, hjust = 1, size =15), 
         axis.text.y = element_text(size = 15), 
-        axis.title.x = element_blank(),  
+        axis.title.x = element_text(face = "bold",size = 15),  
         axis.title.y = element_text(face = "bold",size = 15),
         legend.text = element_text(size = 15),
         legend.title = element_text(size = 19))  +
@@ -36,13 +39,13 @@ plot <- ggplot(data_long, aes(x = Method, y = Percentage, fill = Category)) +
   geom_segment(aes(x = 4.5, xend = 9.5, y = -5, yend = -5), color = "black") +
   
   
-
+  
   geom_segment(aes(x = 1.5, xend = 1.5, y = -10, yend = 0), color = "black", size = 0.5) +  
   geom_segment(aes(x = 6.5, xend = 6.5, y = -10, yend = 0), color = "black", size = 0.5) +  
   geom_segment(aes(x = 9.5, xend = 9.5, y = -10, yend = 0), color = "black", size = 0.5) +
   theme(axis.text.x = element_text(size = 12, angle = 45, hjust = 1)) +
-  annotate("text", x = 4.2, y = -8, label = "Reference guided", vjust = 0.5, size = 6) +
-  annotate("text", x = 8, y = -8, label = "De novo", vjust = 0.5, size = 6, fontface = 'italic')
+  annotate("text", x = 4.2, y = -8, label = "Reference guided", vjust = 0.5, size = 5) +
+  annotate("text", x = 8, y = -8, label = "De novo", vjust = 0.5, size = 5, fontface = 'italic')
 plot
 ggsave(filename = "splicing-profiles.tiff", plot = plot, device = 'tiff', width= 7.66, height= 6.22, dpi = 350)
 
