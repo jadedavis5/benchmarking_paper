35f0dcbf4c91fde3fb86b2686c2fd3847a8f07bc
jadedavis5 <jdavis@setonix-03.head.cm.setonix.pawsey.org.au>
2025-10-24 12:38:08 +0800

    update upset plot

--- Annotation_analysis/UpsetPlot/upset-plot.R
+++ Annotation_analysis/UpsetPlot/upset-plot.R
@@ -1,33 +1,41 @@
 #install.packages("UpSetR")
 library(UpSetR)
+library(grid)
 
-##Ref guided
 tracking <- read.table("refguided-gffcmp.tracking", header = FALSE, sep="\t", stringsAsFactors = FALSE)
 
-colnames(tracking) <- c("frag_id","locus","ref-gene-id","class-code","FLAMES","BAMBU","FLAIR","IsoQuant","StringTie3", "RGT Planet")
+colnames(tracking) <- c("frag_id","locus","ref-gene-id","class-code","FLAMES","Bambu","FLAIR","IsoQuant","StringTie3", "RGT Planet")
 tools <- tracking[,5:10]
 tools_bin <- as.data.frame(lapply(tools, function(x) ifelse(x == "-", 0, 1))) #record presence or absence 
 rownames(tools_bin) <- sub("\\|.*", "", tracking$frag_id)
 tools_list <- lapply(tools_bin, function(col) rownames(tools_bin)[col == 1])
 
 #Make plot
-upset(fromList(tools_list), order.by = "freq", sets=names(tools_list))
+png(filename = "refguided-upset-plot.png", units = 'in', width = 9, height = 5, res = 350)
+upset(fromList(tools_list), order.by = "freq", sets=names(tools_list), 
+      text.scale = c(1.3, 1.5, 1.3, 0.9, 1.3, 1), sets.x.label = "Total transcripts")
+
+dev.off()
 
 #Look at examples where RGT Planet has unique transcripts
 library(dplyr)
-RGT_unique <- tools_bin %>%
-  filter(RGTPlanet == 1 & rowSums(select(.,-RGTPlanet)) == 0)
+#RGT_unique <- tools_bin %>%
+#  filter(RGT Planet == 1 & rowSums(select(.,-RGT Planet)) == 0)
 
 
 
 ##De novo
 tracking <- read.table("denovo-gffcmp.tracking", header = FALSE, sep="\t", stringsAsFactors = FALSE)
 
-colnames(tracking) <- c("frag_id","locus","ref-gene-id","class-code","BAMBU","IsoQuant","StringTie3")
+colnames(tracking) <- c("frag_id","locus","ref-gene-id","class-code","Bambu","IsoQuant","StringTie3")
 tools <- tracking[,5:7]
 tools_bin <- as.data.frame(lapply(tools, function(x) ifelse(x == "-", 0, 1))) #record presence or absence 
 rownames(tools_bin) <- sub("\\|.*", "", tracking$frag_id)
 tools_list <- lapply(tools_bin, function(col) rownames(tools_bin)[col == 1])
 
 #Make plot
-upset(fromList(tools_list), order.by = "freq", sets=names(tools_list))
+png(filename = "denovo-upset-plot.png", units = "in", width = 9, height = 5, res = 350)
+upset(fromList(tools_list), order.by = "freq", sets=names(tools_list), 
+      text.scale = c(1.3, 1.5, 1.3, 1, 1.3, 1), sets.x.label = "Total transcripts")
+dev.off()
+
